61d49208a338740c871c7b656fed26a12708cd8c
Michael Vattuone <mvattuone@gmail.com>
2025-11-04 16:31:33 -0500

    Add airship project

--- constants.js
+++ constants.js
@@ -1,4 +1,9 @@
 export const SLIDE_TRANSITION_DURATION = 400;
 
 export const PROJECTS = [
+  {
+    id: 'airship',
+    root: '#app',
+    remote: 'https://mvattuone.github.io/airship/airship.bundle.js',
+  },
 ];
